c9cece744da2f9fe301a86a0f159896ce8f0d562
adenoidhynkel <martin_roentgen@yahoo.com>
2018-11-29 14:51:31 -0500

    added callout on success or failure of adding entry

--- config__query.php
+++ config__query.php
@@ -3,8 +3,8 @@
 	// You can change the string in quotes to whatever query
 	// you'd like to run against your tables
 
-	$sqlEmployees = "SELECT employee_id, CONCAT(first_name, ' ', last_name) AS name FROM bakery_employees";
-	$sqlLocations = "SELECT * FROM bakery_locations";
-	$sqlTimeTable = "SELECT * FROM schedule";
+	$sqlEmployees = "SELECT employee_id, CONCAT(employee_first_name, ' ', employee_last_name) AS name FROM employee";
+	$sqlLocations = "SELECT * FROM bakery";
+	$sqlTimeTable = "SELECT * FROM weekly_schedule";
 
 ?>
--- index.php
+++ index.php
@@ -11,14 +11,24 @@
 <?php include("template__head.php"); ?>
 
 <body>
-    <div class="grid-container"> 
-
+    <div class="grid-container">
         <!--   Title    -->
         <div class="grid-x">
             <div class="cell small-12"><h1 class="h1 text-center">Schedule</h1></div>
         </div>
         <div class="grid-x">
             <div class="cell large-8 large-offset-0 medium-10 medium-offset-1">
+              <?php
+              // this will generate a call out message for status messages
+              if(isset($_GET['p'])):
+                $status = $_GET['p'];
+                if($status == "success"):
+                  echo '<p class="callout success" data-closable="slide-out-right">Employee Successfully Added!</button></p>';
+                else:
+                  echo '<p class="callout alert" data-closable="slide-out-right">Failed to add, an unknown error has occurred!</button></p>';
+                endif;
+              endif;
+              ?>
                 <table class="schedule-table" id="scheduleTable">
                     <thead>
                         <tr>
@@ -26,14 +36,14 @@
                         </tr>
                         <!-- Column headings -->
 
-                        <!-- 
+                        <!--
 
                             These are hardcoded, which isn't ideal.
                             This was done because the actual columns are misaligned
                             with the type of HTML inputs we're using - a datepicker and
                             two timepickers, rather than 2 "datetime" inputs.
 
-                            That said, if you want to use a query to output the 
+                            That said, if you want to use a query to output the
                             table columns, it could look something like this:
                             SELECT COLUMN_NAME
                                 FROM INFORMATION_SCHEMA.COLUMNS
@@ -56,7 +66,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php 
+                        <?php
                         // Checks to see if the query succeeds
 
                         // The $link variable comes from config__connection.php
@@ -69,14 +79,16 @@
                                 // Loops for each row in the database table,
                                 // outputting the column data, wrapped in
                                 // html tags for the html table.
+
+                                // I changed the values to match my database
                                 while($row = mysqli_fetch_array($result)){
                                     echo "<tr>";
                                     echo "<td>" . $row['name'] . "</td>";
-                                    echo "<td>" . $row['day'] . "</td>";
-                                    echo "<td>" . $row['start'] . "</td>";
-                                    echo "<td>" . $row['end'] . "</td>";
-                                    echo "<td>" . $row['address'] . "</td>";
-                                    echo "<td>" . $row['employee_type'] . "</td>";
+                                    echo "<td>" . $row['shift_start_day'] . "</td>";
+                                    echo "<td>" . $row['start_time'] . "</td>";
+                                    echo "<td>" . $row['end_time'] . "</td>";
+                                    echo "<td>" . $row['bakery_address'] . "</td>";
+                                    echo "<td>" . $row['employee_description'] . "</td>";
                                     echo "</tr>";
                                 }
                                 // Free result set
@@ -87,46 +99,46 @@
                         } else{
                             echo "ERROR: Not able to execute $sql. " . mysqli_error($link);
                         }
-             
+
                         ?>
                     </tbody>
                 </table>
             </div>
 
         <div class="cell large-3 large-offset-1 medium-10 medium-offset-1">
-        <!-- 
-            
+        <!--
+
             The PHP code that will run when we hit the submit button
             inside our form.
 
-            Forms have a lot of implicit and opinionated default behaviour - 
+            Forms have a lot of implicit and opinionated default behaviour -
             they're by far the most 'finicky' aspect of HTML, both
             in terms of the DOM API and styling.
 
-            It's a great topic to do some further research on: 
+            It's a great topic to do some further research on:
             https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms
 
          -->
-            <?php 
+            <?php
                 // Define variables and initialize with empty values
                 $employee = $location = $day = $start_time = $end_time = "";
-                 
+
                 // Processing form data when form is submitted
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                     // $_POST["employee"] is the value of
-                    // the input element that has the 
+                    // the input element that has the
                     // attribute name="employee"
                     $input_employee = trim($_POST["employee"]);
                     $employee = $input_employee;
 
                     // $_POST["location"] is the value of
-                    // the input element that has the 
+                    // the input element that has the
                     // attribute name="location"
                     // and so on...
                     $input_location = trim($_POST["location"]);
                     $location = $input_location;
-                    
+
                     $input_day = trim($_POST["day"]);
                     $day = $input_day;
 
@@ -135,83 +147,91 @@
 
                     $input_end_time = trim($_POST["end_time"]);
                     $end_time = $input_end_time;
-                    
+
                     // Prepare an insert statement
                     // The question marks will be replaced
                     // by the bind variables
-                    // 
+                    //
                     // Note that we don't need to pass a primary_key
                     // if the key is set to autoincrement in our table
-                    $sql = "INSERT INTO bakery_shifts (employee_id, location_id, start_time, end_time) VALUES (?, ?, ?, ?)";
-                     
+                    $sql = "INSERT INTO employee_shift (employee_id, bakery_id, shift_start_day, shift_end_day, shift_start_time, shift_end_time) VALUES (?, ?, ?, ?, ?, ?)";
+
                     if($stmt = mysqli_prepare($link, $sql)){
                         // Bind variables to the prepared statement as parameters
                         // "ssss" stands for the four strings
                         // These are PHP data types, so we don't need
                         // to worry about date datatypes
-                        // 
+                        //
                         // You can see a table of the four data types
                         // accepted by mysqli_stmt_bind_param here:
                         // http://php.net/manual/en/mysqli-stmt.bind-param.php
-                        mysqli_stmt_bind_param($stmt, "ssss", $param_employee, $param_location, $param_start_time, $param_end_time);
-                        
-                        // Set parameters - 
-                        // Note how we align the values from the 
+
+                        // I added 2 more columns
+                        mysqli_stmt_bind_param($stmt, "ssssss", $param_employee, $param_location, $param_start_day, $param_end_day , $param_start_time, $param_end_time);
+
+                        // Set parameters -
+                        // Note how we align the values from the
                         // 3 html inputs - 2 time and one date -
                         // with the 2 database fields we need to populate
-                        // 
+                        //
                         // Date formatting and conversion is pretty brutal in PHP,
                         // so it ended up being less work simply formatting
                         // our input data as a string that's recognizable to MySQL.
                         // The default HTML date and time inputs have similar formatting -
                         // YYYY-MM-DD and a 24 hour clock. All we have to do here
                         // is add a space between, and milliseconds to the time.
-                        // 
+                        //
                         // This is lucky, but it still took a good deal of var_dump()
                         // to get right.
                         $param_employee = $employee;
                         $param_location = $location;
-                        $param_start_time = $day . ' ' .  $start_time . ':00';
-                        $param_end_time = $day . ' ' . $end_time . ':00';
-                        
-                        
+                        $param_start_day = $day;
+                        $param_end_day = $day . "+ INTERVAL 1 WEEK";
+                        $param_start_time = $day . $start_time . ':00';
+                        $param_end_time = $day . $end_time . ':00';
+
                         // Attempt to execute the prepared statement
                         if(mysqli_stmt_execute($stmt)){
-                            // Records created successfully. 
+                            // Records created successfully.
                             // Refreshing the page.
                             // This could also be set to a "success" page
-                            header("location: index.php");
+
+                            // added a success query string so I can retrive it to show the message
+                            header("location: index.php?p=success");
                             exit();
                         } else{
-                            echo "Something went wrong. Please try again later.";
+                            // if it fails
+                            header("location: index.php?p=fail");
+                            exit();
+                          //echo "Something went wrong. Please try again later.";
                         }
                     }
-                     
+
                     // Close statement
                     mysqli_stmt_close($stmt);
                 }
             ?>
-            <!-- 
+            <!--
                 The submission form.
              -->
             <form class="schedule-form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                 <fieldset>
                     <legend>Add a shift</legend>
-                
+
                     <div class="grid-x grid-padding-x">
 
                         <div class="medium-12 cell">
                             <label>Employee
                                 <select name="employee" id="employeeDatalist" required>
-                                    <!-- 
+                                    <!--
                                         A database request so we can have
                                         one select option for each employee.
 
-                                        Note that we need to set the option value to 
+                                        Note that we need to set the option value to
                                         the employee_id, as that's what goes into the shifts table.
                                      -->
                                     <option value="" selected disabled hidden></option>
-                                    <?php 
+                                    <?php
                                     if($result = mysqli_query($link, $sqlEmployees)){
                                         if(mysqli_num_rows($result) > 0){
                                             while($row = mysqli_fetch_array($result)){
@@ -235,11 +255,12 @@
                                 <select name="location" id="locationDatalist" required>
                                     <!-- Same idea here. -->
                                     <option value="" selected disabled hidden></option>
-                                    <?php 
+                                    <?php
                                     if($result = mysqli_query($link, $sqlLocations)){
                                         if(mysqli_num_rows($result) > 0){
                                             while($row = mysqli_fetch_array($result)){
-                                                echo "<option value='" . $row['location_id'] . "'>" . $row['address'] . "</option>";
+                                              // I changed the $row['bakery_id'], $row['bakery_name'], $row['bakery_address']
+                                                echo "<option value='" . $row[0] . "'>" . $row[1] . ' - ' . $row[2] . "</option>";
                                             }
                                             // Free result set
                                             mysqli_free_result($result);
@@ -255,8 +276,8 @@
                         </div>
                     </div>
 
-                    <!-- 
-                    
+                    <!--
+
                         Let's talk about validation for a moment.
                         We can see that on the page, we have client-side validation.
 
@@ -268,7 +289,7 @@
                         Support for the Native HTML validation API is pretty great:
                         https://caniuse.com/#feat=form-validation
 
-                        It means that people can have their mistakes caught without 
+                        It means that people can have their mistakes caught without
                         adding expensive scripting to the page, or having to wait for
                         the server to come back and tell them they screwed up.
 
@@ -317,10 +338,10 @@
         // Close connection
         mysqli_close($link);
     ?>
-<!-- 
+<!--
 
     Down in the footer, I include two javascript operations
-    1. Setting the date inputs' min and max values to 
+    1. Setting the date inputs' min and max values to
         today and 1 year from today, respectively
     2. My friend Tristen's library for table sorting
         because nepotism :)
